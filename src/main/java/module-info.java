2e2a598f2391637ada016dffaaa75e404cd03ec2
Mybiss55 <44406758+Mybiss55@users.noreply.github.com>
2023-07-28 21:20:30 -0400

    Added Maven Dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.assignment2gc200486396 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
+    requires java.net.http;
 
-
-    opens com.example.assignment2gc200486396 to javafx.fxml;
+    opens com.example.assignment2gc200486396 to javafx.fxml, com.google.gson;
     exports com.example.assignment2gc200486396;
 }
