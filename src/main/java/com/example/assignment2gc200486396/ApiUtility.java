88d2049af67f548144729a9274505340c595f6b4
Mybiss55 <44406758+Mybiss55@users.noreply.github.com>
2023-07-30 11:59:52 -0400

    Added Second API Utility: getDataFromAPI

--- src/main/java/com/example/assignment2gc200486396/ApiUtility.java
+++ src/main/java/com/example/assignment2gc200486396/ApiUtility.java
@@ -4,6 +4,11 @@ import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
 import java.io.FileReader;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ApiUtility {
     // parse JSON response and return ApiResponse object
@@ -15,12 +20,29 @@ public class ApiUtility {
                 JsonReader jsonReader = new JsonReader(fileReader);
                 ){
             apiResponse = gson.fromJson(jsonReader, ApiResponse.class);
-            System.out.println(apiResponse);
         } catch (Exception e) {
             System.out.println("Error parsing JSON response");
             e.printStackTrace();
         }
 
         return apiResponse;
+    }
+
+    public static ApiResponse getDataFromAPI(String appId){
+        String url = "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=" + appId + "&count=3&maxlength=300&format=json";
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(java.net.URI.create(url)).build();
+
+
+        try{
+            HttpResponse<Path> response = httpClient.send(httpRequest, HttpResponse
+                    .BodyHandlers
+                    .ofFile(Paths.get("javaApiFetched.json")));
+        } catch (Exception e){
+            System.out.println("Error fetching data from API");
+            e.printStackTrace();
+        }
+        return ApiUtility.getDataFromFile("javaApiFetched.json");
     }
 }
