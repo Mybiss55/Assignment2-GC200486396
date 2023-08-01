92edc84daac309c06295d038b25ba66bc277fbbb
Mybiss55 <44406758+Mybiss55@users.noreply.github.com>
2023-08-01 13:27:30 -0400

    Quick and Dirty Method of getting rid of the rouge links.

--- src/main/java/com/example/assignment2gc200486396/NewsController.java
+++ src/main/java/com/example/assignment2gc200486396/NewsController.java
@@ -26,14 +26,22 @@ public class NewsController {
     @FXML
     void showNews(ActionEvent event) {
         String appId = inputId.getText();
+        appId = appId.trim();
         ApiResponse response = ApiUtility.getDataFromAPI(appId);
-        String contentText = response.getAppNews().getNewsitems().get(0).getContents();
+        String contentText = " ";
+        try {
+            contentText = response.getAppNews().getNewsitems().get(0).getContents();
+        }catch (Exception e){
+            contentText = "No news found for this game";
+        }
 
 
-        // Get rid of any images in the content
-        String pattern = ".*\\.(png|jpg)";
+
+        // Get rid of any images in the content and the <a> tags
+        // TODO - Find a way to display the links rather than removing them
+        String pattern = ".*\\.(png|jpg|gif|bmp|jpeg|PNG|JPG|GIF|BMP|JPEG) | <a[^>]*>.*?</a>"; // <[^\n<>]+>
         Pattern regex = Pattern.compile(pattern);
-        contentText = regex.matcher(contentText).replaceAll("");
+        contentText = regex.matcher(contentText).replaceAll(" ");
 
         // Change Author
         String authorText = response.getAppNews().getNewsitems().get(0).getAuthor();
@@ -42,11 +50,6 @@ public class NewsController {
         // Change Title
         String titleText = response.getAppNews().getNewsitems().get(0).getTitle();
         newsTitle.setText(titleText);
-
-
-
-
-
         contentArea.setText(contentText);
         contentArea.wrapTextProperty().setValue(true);
     }
