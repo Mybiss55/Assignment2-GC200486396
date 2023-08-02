c256e7b0023c36435d9935510a8c3d52892db679
Mybiss55 <44406758+Mybiss55@users.noreply.github.com>
2023-08-02 15:23:49 -0400

    Added WebView, (Fixes #1)

--- src/main/java/com/example/assignment2gc200486396/NewsController.java
+++ src/main/java/com/example/assignment2gc200486396/NewsController.java
@@ -5,12 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.web.WebView;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class NewsController {
 
+    @FXML
+    private WebView webView;
     @FXML
     private TextArea contentArea;
 
@@ -37,11 +40,12 @@ public class NewsController {
 
 
 
+
         // Get rid of any images in the content and the <a> tags
         // TODO - Find a way to display the links rather than removing them
-        String pattern = ".*\\.(png|jpg|gif|bmp|jpeg|PNG|JPG|GIF|BMP|JPEG) | <a[^>]*>.*?</a>"; // <[^\n<>]+>
-        Pattern regex = Pattern.compile(pattern);
-        contentText = regex.matcher(contentText).replaceAll(" ");
+//        String pattern = ".*\\.(png|jpg|gif|bmp|jpeg|PNG|JPG|GIF|BMP|JPEG) | <a[^>]*>.*?</a>"; // <[^\n<>]+>
+//        Pattern regex = Pattern.compile(pattern);
+//        contentText = regex.matcher(contentText).replaceAll(" ");
 
         // Change Author
         String authorText = response.getAppNews().getNewsitems().get(0).getAuthor();
@@ -50,8 +54,10 @@ public class NewsController {
         // Change Title
         String titleText = response.getAppNews().getNewsitems().get(0).getTitle();
         newsTitle.setText(titleText);
-        contentArea.setText(contentText);
-        contentArea.wrapTextProperty().setValue(true);
+//        contentArea.setText(contentText);
+//        contentArea.wrapTextProperty().setValue(true);
+
+        webView.getEngine().loadContent(contentText);
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.assignment2gc200486396 {
     requires javafx.fxml;
     requires com.google.gson;
     requires java.net.http;
+    requires javafx.web;
 
     opens com.example.assignment2gc200486396 to javafx.fxml, com.google.gson;
     exports com.example.assignment2gc200486396;
