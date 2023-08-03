83b1b68c7f7dedb32e33cd4792d2ac2b80fd4a22
Mybiss55 <44406758+Mybiss55@users.noreply.github.com>
2023-08-03 14:47:34 -0400

    Fixed CSS Error Added Icon

--- src/main/java/com/example/assignment2gc200486396/HelloApplication.java
+++ src/main/java/com/example/assignment2gc200486396/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.assignment2gc200486396;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
@@ -14,6 +15,7 @@ public class HelloApplication extends Application{
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("news-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add("styles.css");
+        stage.getIcons().add(new Image("file:src/main/resources/com/example/assignment2gc200486396/SG.png"));
         stage.setTitle("Game News");
         stage.setScene(scene);
         stage.show();
